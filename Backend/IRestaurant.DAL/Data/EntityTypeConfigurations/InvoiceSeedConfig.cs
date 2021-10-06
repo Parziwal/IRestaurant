2b4104c7ac61d50340564714fed207315bf3be66
Stedra Kristóf <stedra99@gmail.com>
2021-10-06 16:54:33 +0200

    Add order seed data

--- Backend/IRestaurant.DAL/Data/EntityTypeConfigurations/InvoiceSeedConfig.cs
+++ Backend/IRestaurant.DAL/Data/EntityTypeConfigurations/InvoiceSeedConfig.cs
@@ -55,6 +55,118 @@ namespace IRestaurant.DAL.Data.EntityTypeConfigurations
                     OrderId = 6,
                     UserFullName = "Carson Alexander",
                     RestaurantName = "Twentysix Budapest",
+                },
+                new Invoice
+                {
+                    Id = 7,
+                    OrderId = 7,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 8,
+                    OrderId = 8,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 9,
+                    OrderId = 9,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 10,
+                    OrderId = 10,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 11,
+                    OrderId = 11,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Princess Bakery&Bistro Örs vezér tér",
+                },
+                new Invoice
+                {
+                    Id = 12,
+                    OrderId = 12,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Twentysix Budapest",
+                },
+                new Invoice
+                {
+                    Id = 13,
+                    OrderId = 13,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 14,
+                    OrderId = 14,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 15,
+                    OrderId = 15,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 16,
+                    OrderId = 16,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 17,
+                    OrderId = 17,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Princess Bakery&Bistro Örs vezér tér",
+                },
+                new Invoice
+                {
+                    Id = 18,
+                    OrderId = 18,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Twentysix Budapest",
+                },
+                new Invoice
+                {
+                    Id = 19,
+                    OrderId = 19,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 20,
+                    OrderId = 20,
+                    UserFullName = "Carson Alexander",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 21,
+                    OrderId = 21,
+                    UserFullName = "Bács Imre",
+                    RestaurantName = "Trófea Grill Étterem - Király",
+                },
+                new Invoice
+                {
+                    Id = 22,
+                    OrderId = 22,
+                    UserFullName = "Bács Imre",
+                    RestaurantName = "Trófea Grill Étterem - Király",
                 }
             );
 
@@ -106,6 +218,134 @@ namespace IRestaurant.DAL.Data.EntityTypeConfigurations
                     ZipCode = 1060,
                     Street = "Kossuth utca 30",
                     PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 7,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 8,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 9,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 10,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 11,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 12,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 13,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 14,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 15,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 16,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 17,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 18,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 19,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 20,
+                    City = "Budapest",
+                    ZipCode = 1060,
+                    Street = "Kossuth utca 30",
+                    PhoneNumber = "06201214561"
+                },
+                new
+                {
+                    InvoiceId = 21,
+                    City = "Budapest",
+                    ZipCode = 1017,
+                    Street = "Petőfi utca 3",
+                    PhoneNumber = "06301451567"
+                },
+                new
+                {
+                    InvoiceId = 22,
+                    City = "Budapest",
+                    ZipCode = 1017,
+                    Street = "Petőfi utca 3",
+                    PhoneNumber = "06301451567"
                 }
             );
 
@@ -157,6 +397,134 @@ namespace IRestaurant.DAL.Data.EntityTypeConfigurations
                     ZipCode = 1011,
                     Street = "Király utca 26",
                     PhoneNumber = "06301233456"
+                },
+                new
+                {
+                    InvoiceId = 7,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 8,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 9,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 10,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 11,
+                    City = "Budapest",
+                    ZipCode = 1148,
+                    Street = "Örs vezér tere 1",
+                    PhoneNumber = "06305412567"
+                },
+                new
+                {
+                    InvoiceId = 12,
+                    City = "Budapest",
+                    ZipCode = 1011,
+                    Street = "Király utca 26",
+                    PhoneNumber = "06301233456"
+                },
+                new
+                {
+                    InvoiceId = 13,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 14,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 15,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 16,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 17,
+                    City = "Budapest",
+                    ZipCode = 1148,
+                    Street = "Örs vezér tere 1",
+                    PhoneNumber = "06305412567"
+                },
+                new
+                {
+                    InvoiceId = 18,
+                    City = "Budapest",
+                    ZipCode = 1011,
+                    Street = "Király utca 26",
+                    PhoneNumber = "06301233456"
+                },
+                new
+                {
+                    InvoiceId = 19,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 20,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 21,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
+                },
+                new
+                {
+                    InvoiceId = 22,
+                    City = "Budapest",
+                    ZipCode = 1061,
+                    Street = "Király utca 30",
+                    PhoneNumber = "06301234567"
                 }
             );
         }
--- Backend/IRestaurant.DAL/Data/EntityTypeConfigurations/OrderSeedConfig.cs
+++ Backend/IRestaurant.DAL/Data/EntityTypeConfigurations/OrderSeedConfig.cs
@@ -61,6 +61,135 @@ namespace IRestaurant.DAL.Data.EntityTypeConfigurations
                     CreatedAt = DateTime.Now,
                     Status = OrderStatus.PROCESSING,
                     PreferredDeliveryDate = DateTime.Now.AddDays(1)
+                },
+                new Order
+                {
+                    Id = 7,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-30),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-25)
+                },
+                new Order
+                {
+                    Id = 8,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-35),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-33)
+                },
+                new Order
+                {
+                    Id = 9,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-40),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddHours(-38)
+                },
+                new Order
+                {
+                    Id = 10,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-43),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-41)
+                },
+                new Order
+                {
+                    Id = 11,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-50),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-45)
+                },
+                new Order
+                {
+                    Id = 12,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-55),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-52)
+                },
+                new Order
+                {
+                    Id = 13,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-60),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-59)
+                },
+                new Order
+                {
+                    Id = 14,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-64),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddHours(-61)
+                },
+                new Order
+                {
+                    Id = 15,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-69),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-67)
+                },
+                new Order
+                {
+                    Id = 16,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-374),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-370)
+                },
+                new Order
+                {
+                    Id = 17,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-385),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-381)
+                },
+                new Order
+                {
+                    Id = 18,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-392),
+                    Status = OrderStatus.CANCELLED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-390)
+                },
+                new Order
+                {
+                    Id = 19,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-397),
+                    Status = OrderStatus.CANCELLED,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(-395)
+                },
+                new Order
+                {
+                    Id = 20,
+                    UserId = "475c5e32-049c-4d7b-a963-02ebdc15a94b",
+                    CreatedAt = DateTime.Now.AddDays(-705),
+                    Status = OrderStatus.CANCELLED,
+                    PreferredDeliveryDate = DateTime.Now.AddHours(-700)
+                },
+
+                new Order
+                {
+                    Id = 21,
+                    UserId = "cb35b922-5a91-4949-94e6-47a2d6f82d93",
+                    CreatedAt = DateTime.Now.AddDays(-1),
+                    Status = OrderStatus.PROCESSING,
+                    PreferredDeliveryDate = DateTime.Now.AddDays(5)
+                },
+                new Order
+                {
+                    Id = 22,
+                    UserId = "cb35b922-5a91-4949-94e6-47a2d6f82d93",
+                    CreatedAt = DateTime.Now.AddDays(-378),
+                    Status = OrderStatus.DELIVERED,
+                    PreferredDeliveryDate = DateTime.Now.AddHours(-372)
                 }
             );
         }
