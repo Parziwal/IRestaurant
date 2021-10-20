d6bb927ab8046c28e7303a51d51d25d95d975c50
Stedra Kristóf <stedra99@gmail.com>
2021-10-20 13:05:09 +0200

    Fix problems based on tests

--- Backend/IRestaurant.DAL/Repositories/Implementations/RestaurantRepository.cs
+++ Backend/IRestaurant.DAL/Repositories/Implementations/RestaurantRepository.cs
@@ -237,7 +237,7 @@ namespace IRestaurant.DAL.Repositories.Implementations
 
         /// <summary>
         /// A megadott azonosítójú étterem eltávolítása törlése a vendég kedvencei közül.
-        /// Ha a felhasználó kedvencei között az megadott étterem nem található, akkor kivételt dobunk.
+        /// Ha a felhasználó, vagy annak kedvencei között az megadott étterem nem található, akkor kivételt dobunk.
         /// </summary>
         /// <param name="restaurantId">Az étterem azonosítója.</param>
         /// <param name="guestId">A vendég azonosítója.</param>
